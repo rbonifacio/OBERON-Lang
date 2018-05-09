2e470b48c0b3538145b91f92bd9e141beb0c0bce
Luiz <luiz@lexCorp>
2018-05-08 21:18:45 -0300

    Implementation of arithmetic binary expressions using macros to create headers and the source. Fixed some issues of memory leak caused by not deleting objects created. Added new tests for arithmetic binary expressions such as srl (shift left logically), sll and xor Changed int to int64_t from cstdint, this allows us to know exatally the size of integer.

--- include/BinExpression.hpp
+++ include/BinExpression.hpp
@@ -12,13 +12,24 @@ namespace OberonLang {
   public:
     BinExpression(Expression* l, Expression* r) { lhs = l; rhs = r; };
     virtual Value* eval() = 0;
-  };
-
-  class AddExpression : public BinExpression {
-  public:
-    AddExpression(Expression* l, Expression* r) : BinExpression(l, r) {};
-    Value* eval();
-  };
+  };  
+  
+  #define __headerArithBinExp(infix)\
+  class infix##Expression : public BinExpression {\
+  public:\
+    infix##Expression(Expression* l, Expression* r) : BinExpression(l, r) {};\
+    Value* eval();\
+  };\
+  
+  __headerArithBinExp(Add);  // ~ AddExpression
+  __headerArithBinExp(Sub);  // ~ SubExpression
+  __headerArithBinExp(Mult); // ~ MultExpression
+  __headerArithBinExp(Div);  // ~ DivExpression
+  __headerArithBinExp(Srl);  // ~ SrlExpression
+  __headerArithBinExp(Sll);  // ~ SllExpression
+  __headerArithBinExp(And);  // ~ AndExpression
+  __headerArithBinExp(Or);   // ~ OrExpression
+  __headerArithBinExp(Xor);  // ~ XorExpression
     
 }
 #endif 
--- src/BinExpression.cpp
+++ src/BinExpression.cpp
@@ -3,11 +3,29 @@
 
 namespace OberonLang {
   
-  Value* AddExpression::eval() {
-    int v1 = ((IntValue*)lhs->eval())->value();
-    int v2 = ((IntValue*)rhs->eval())->value();
-
-    return new IntValue(v1 + v2);
-  }
+  /* 
+     Macro que implementa expressões binárias aritméticas simples.
+     infix: o nome da expressão, esse nome será usado como infixo
+     operator: a operação equivalente em C++
+     valueType: o tipo dos dados que serão manipulados
+   */
+   
+  #define __implementArithBinExp(infix, operator, valueType)\
+  Value* infix##Expression::eval() {\
+    int64_t v1 = ((valueType*)lhs->eval())->value();\
+    int64_t v2 = ((valueType*)rhs->eval())->value();\
+    \
+    return new valueType(v1 operator v2);\
+  }\
+  
+  __implementArithBinExp(Add,  +, IntValue);      // ~ AddExpression
+  __implementArithBinExp(Sub,  -, IntValue);      // ~ SubExpression
+  __implementArithBinExp(Mult, *, IntValue);      // ~ MultExpression
+  __implementArithBinExp(Div,  /, IntValue);      // ~ DivExpression
+  __implementArithBinExp(Srl, >>, IntValue);      // ~ SrlExpression
+  __implementArithBinExp(Sll, <<, IntValue);      // ~ SllExpression
+  __implementArithBinExp(And, &&, BooleanValue);  // ~ AndExpression
+  __implementArithBinExp(Or,  ||, BooleanValue);  // ~ OrExpression
+  __implementArithBinExp(Xor,  ^, BooleanValue);  // ~ XorExpression
   
 }
