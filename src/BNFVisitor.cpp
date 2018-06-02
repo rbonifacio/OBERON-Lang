6a82a9bf8868e37d299047fb79116e95c1a17e31
Rodrigo B Almeida <rbonifacio@Rodrigos-iMac.local>
2018-06-02 19:17:04 -0300

    a visitor to parse the generated OBERON BNFC AST into our AST

---
+++ src/BNFVisitor.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   BNFVisitor.cpp
+ * Author: rbonifacio
+ * 
+ * Created on June 2, 2018, 11:38 AM
+ */
+
+#include "BNFVisitor.hpp"
+
+namespace OberonLang {
+    BNFVisitor::BNFVisitor() {
+    }
+
+    BNFVisitor::BNFVisitor(const BNFVisitor& orig) {
+    }
+
+    BNFVisitor::~BNFVisitor() {
+    }
+}
+
