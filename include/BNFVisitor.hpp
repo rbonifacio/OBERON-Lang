d4f209c5b6854f474e66d41496851a8c18da1616
labm1997 <labm1997@gmail.com>
2018-06-02 21:43:27 -0300

    Some visitors have been implemented: 	TInt, TBool, Decl, ELt, EGt, EEq, ENEq, EAdd, ESub, EMul, EDiv, EOr, EAnd, EInt Not satisfied with the way I'm "returning" from each accept.

--- include/BNFVisitor.hpp
+++ include/BNFVisitor.hpp
@@ -1,13 +1,19 @@
 #ifndef BNFVISITOR_H
 #define BNFVISITOR_H
 
+#include <vector>
 #include "Absyn.H"
+#include "Expression.hpp"
+#include "BinExpression.hpp"
+#include "Types.hpp"
+#include "Declaration.hpp"
 
 namespace OberonLang {
   class BNFVisitor : public Visitor {
   public:
     BNFVisitor();
     BNFVisitor(const BNFVisitor& orig);
+    
     
     void visitModuleDec(ModuleDec *p) { }
     void visitType(Type *p) { }
@@ -17,25 +23,25 @@ namespace OberonLang {
     void visitExp(Exp *p) { }
     void visitStmt(Stmt *p) { }
     void visitModule(Module *p) { }
-    void visitTInt(TInt *p) { }
-    void visitTBool(TBool *p) { }
-    void visitDecl(Decl *p) { }
+    void visitTInt(TInt *p);
+    void visitTBool(TBool *p);
+    void visitDecl(Decl *p);
     void visitFPDecl(FPDecl *p) { }
     void visitPDec(PDec *p) { }
-    void visitELt(ELt *p) { }
-    void visitEGt(EGt *p) { }
-    void visitEEq(EEq *p) { }
-    void visitENEq(ENEq *p) { }
-    void visitEAdd(EAdd *p) { }
-    void visitESub(ESub *p) { }
-    void visitEMul(EMul *p) { }
-    void visitEDiv(EDiv *p) { }
-    void visitEOr(EOr *p) { }
-    void visitEAnd(EAnd *p) { }
+    void visitELt(ELt *p);
+    void visitEGt(EGt *p);
+    void visitEEq(EEq *p);
+    void visitENEq(ENEq *p);
+    void visitEAdd(EAdd *p);
+    void visitESub(ESub *p);
+    void visitEMul(EMul *p);
+    void visitEDiv(EDiv *p);
+    void visitEOr(EOr *p);
+    void visitEAnd(EAnd *p);
     void visitCall(Call *p) { }
     void visitEVar(EVar *p) { }
     void visitEStr(EStr *p) { }
-    void visitEInt(EInt *p) { }
+    void visitEInt(EInt *p);
     void visitEFalse(EFalse *p) { }
     void visitETrue(ETrue *p) { }
     void visitEDouble(EDouble *p) { }
@@ -46,7 +52,7 @@ namespace OberonLang {
     void visitSIfThen(SIfThen *p) { }
     void visitListExp(ListExp *p) { }
     void visitListStmt(ListStmt *p) { }
-    void visitListIdent(ListIdent *p) { }
+    void visitListIdent(ListIdent *p);
     void visitListVarDec(ListVarDec *p) { }
     void visitListFPmtDec(ListFPmtDec *p) { }
     void visitListProcDec(ListProcDec *p) { }
@@ -54,11 +60,18 @@ namespace OberonLang {
     void visitChar(Char x) { }
     void visitDouble(Double x) { }
     void visitString(String x) { }
-    void visitIdent(Ident x) { }
+    void visitIdent(Ident x);
     
     virtual  ~BNFVisitor();
   private:
+  	
+  	// As visitors can't return, these variables will store the results of a visitor.
+    Expression *visitorReturn;
+    TypesEnum visitorTypeReturn;
+    vector<Declaration> *visitorDeclarationListReturn;
     
+    // When an expression is parsed we don't know if there will be reals
+    bool doubleFlag = 0; // Detects if a double occourred in an expression
   };
 }
 #endif /* BNFVISITOR_HPP */
--- src/BNFVisitor.cpp
+++ src/BNFVisitor.cpp
@@ -22,5 +22,69 @@ namespace OberonLang {
 
     BNFVisitor::~BNFVisitor() {
     }
+    
+    void BNFVisitor::visitTInt(TInt *p) {
+    	visitorTypeReturn = integer;
+    }
+    void BNFVisitor::visitTBool(TBool *p) {
+    	visitorTypeReturn = boolean;
+    }
+    
+    void BNFVisitor::visitDecl(Decl *p) {
+    	p->type_->accept(this); // Returns in visitorTypeReturn
+    	visitorDeclarationListReturn = new vector<Declaration>;
+    	if(p->listident_){
+    		for(
+    			ListIdent::const_iterator element = p->listident_->begin(); 
+    			element != p->listident_->end() ; 
+    			++element
+    			)
+    		{
+    			visitorDeclarationListReturn->push_back(Declaration(visitorTypeReturn, *element));
+    		}
+    	}
+    }
+    
+    #define __implementBNFVisitorBinExpression(BNFClassName, ASTInfix, ASTRealInfix)\
+    void BNFVisitor::visit##BNFClassName(BNFClassName *p) { \
+    	Expression *lhs, *rhs;\
+    	doubleFlag = false;\
+    	p->exp_1->accept(this);\
+    	lhs = visitorReturn;\
+    	p->exp_2->accept(this);\
+    	rhs = visitorReturn;\
+    	if(doubleFlag) visitorReturn = new ASTRealInfix##Expression(lhs, rhs);\
+    	else visitorReturn = new ASTInfix##Expression(lhs, rhs);\
+    }
+    
+    // Those can have real or integers
+    __implementBNFVisitorBinExpression(EAdd, Add, AddReal);     // Implements visitEAdd
+    __implementBNFVisitorBinExpression(ESub, Sub, SubReal);     // Implements visitESub
+    __implementBNFVisitorBinExpression(EMul, Times, TimesReal); // Implements visitETimes
+    __implementBNFVisitorBinExpression(EDiv, Div, DivReal);     // Implements visitEDiv
+    
+    // Those are only integers, we should have relational expressions for real but we don't
+    __implementBNFVisitorBinExpression(EEq, EQ, EQ);     // Implements visitEEq
+    __implementBNFVisitorBinExpression(ENEq, NE, NE);    // Implements visitENEq
+    __implementBNFVisitorBinExpression(ELt, LT, LT);     // Implements visitELt
+    __implementBNFVisitorBinExpression(EGt, GT, GT);     // Implements visitEGt
+    
+    /* Logic binary expressions */
+    __implementBNFVisitorBinExpression(EAnd, And, And);  // Implements visitEAnd
+    __implementBNFVisitorBinExpression(EOr, Or, Or);     // Implements visitEAnd
+    
+    void BNFVisitor::visitEInt(EInt *p) { 
+    	visitorReturn = new IntValue(p->integer_);
+    }
+    
+    void BNFVisitor::visitListIdent(ListIdent *p) {
+    	for(ListIdent::const_iterator element = p->begin() ; element != p->end() ; ++element){
+    		BNFVisitor::visitIdent(*element);
+    	}
+    }
+    
+    void BNFVisitor::visitIdent(Ident x) {
+    	
+    }
 }
 
